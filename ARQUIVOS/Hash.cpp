c08e7cccd2fb0203834512eb00dddd3c7075f394
Sabrina Marinho <92945541+httpsk1b0@users.noreply.github.com>
2022-11-03 23:41:15 -0300

    Update Hash.cpp

--- ARQUIVOS/Hash.cpp
+++ ARQUIVOS/Hash.cpp
@@ -8,7 +8,7 @@
 #include <stdbool.h>
 
 // Tamanho do vetor
-#define SIZE 1024
+#define SIZE 5571
 
 // Vetor de ponterios do tipo dataItem
 typedef dataItem *hash[SIZE];
@@ -24,7 +24,6 @@ void init(hash &H) {
 }
 
 int inserirNovo(hash H, dataItem d) {
-   
     dataItem *copy = (dataItem*)malloc(sizeof(dataItem));
     struct no *novo = (struct no*)malloc(sizeof(struct no));
     *copy = d;
@@ -35,7 +34,7 @@ int inserirNovo(hash H, dataItem d) {
     if (H[resto] == 0) {;
         H[resto] = copy;
         return 0;
-    }else if(H[resto] != 0){
+    } else if(H[resto] != 0){
         novo->prox = H[resto]->inicio;
         H[resto]->inicio = novo;
         return 0;
@@ -44,7 +43,30 @@ int inserirNovo(hash H, dataItem d) {
    return -1;
 }
 
+/*
+void remover(dataItem *d){
+  if (d->inicio->prox == 0){
+    d->inicio = 0;
+  }
+}
+*/
 
+dataItem * buscar(hash H, int id){
+  int resto = id%SIZE;
+  if (H[resto]->key == id) // Caso (1) esteja esteja imaculado
+    return H[resto];    
+  else { // Caso haja colisão
+    struct no *temp = (struct no*)malloc(sizeof(struct no)); 
+    // struct no *temp; // É realmente necessário alocar?
+    temp = H[resto]->inicio; // Variável temporária para fins de iteração
+    while(temp != NULL){ // Enquanto a variável temporária apontar para algo existente continua
+      if(temp->data.key == id) // Volta para o caso (1)
+        return &temp->data;
+      else temp = temp->prox; // Aponta para o próximo na fila
+    }
+  }
+  return NULL;
+}
 
 /*
 void escrever(hash H){
